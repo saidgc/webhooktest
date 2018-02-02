61feaf1e9228e1a861894f99632f76c8aab63d11
ByteMex <said.g@hotmail.com>
2018-02-01 22:02:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -53,27 +53,11 @@ def webhook():
 
 
 def processRequest(req):
+    print(req.get("result").get("action"))
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
+    res = makeWebhookResult(req)
     return res
-
-
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
-
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 def makeWebhookResult(data):
     speech = "Reqs =" + str(data)
